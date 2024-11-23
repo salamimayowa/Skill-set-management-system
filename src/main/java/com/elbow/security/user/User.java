89c45c49f06ee41d3ad21a0b86bcc59c7c6033a3
kingsleynwafor54 <kingsleynwafor54@gmail.com>
2024-11-23 09:04:21 +0100

    setting up you mysql database with springboot

---
+++ src/main/java/com/elbow/security/user/User.java
@@ -0,0 +1,23 @@
+package com.elbow.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
